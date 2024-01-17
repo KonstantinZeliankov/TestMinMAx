91bf7673a5e1bce07ef1889f348f01c3c677a757
KonstantinZelenkov <konstantin.zeliankov@gmail.com>
2024-01-17 11:48:08 +0100

    Massive name changed #

--- Program.cs
+++ Program.cs
@@ -12,23 +12,23 @@
             static void Main(string[] args)
 
             {
-                int[] arraymaxmin = { 6, -5, 0, 9, 66, -86, 798, 6, 11, 10 };
+                int[] arrayMaxMin = { 6, -5, 0, 9, 66, -86, 798, 6, 11, 10 };
                 int max_value_index = 0;
                 int min_value_index = 0;
 
-                for (int i = 0; i < arraymaxmin.Length; i++)
+                for (int i = 0; i < arrayMaxMin.Length; i++)
                 {
-                    int value = arraymaxmin[i];
-                    if (value < arraymaxmin[min_value_index]) { min_value_index = i; }
-                    if (value > arraymaxmin[max_value_index]) { max_value_index = i; }
+                    int value = arrayMaxMin[i];
+                    if (value < arrayMaxMin[min_value_index]) { min_value_index = i; }
+                    if (value > arrayMaxMin[max_value_index]) { max_value_index = i; }
 
                 }
 
-                int temporary_value = arraymaxmin[min_value_index];
-                arraymaxmin[min_value_index] = arraymaxmin[max_value_index];
-                arraymaxmin[max_value_index] = temporary_value;
+                int temporary_value = arrayMaxMin[min_value_index];
+                arrayMaxMin[min_value_index] = arrayMaxMin[max_value_index];
+                arrayMaxMin[max_value_index] = temporary_value;
 
-                PrintArrayToConsole(arraymaxmin);
+                PrintArrayToConsole(arrayMaxMin);
 
             }
     }
